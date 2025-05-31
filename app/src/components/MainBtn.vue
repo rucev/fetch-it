1bb2bcde1a64271d9d73422e11e9cda14e6f17a9
Flors Rueda <ruedaflors@gmail.com>
2025-05-31 13:15:38 +0200

    mend
    refactor buttons #1

--- app/src/components/MainBtn.vue
+++ app/src/components/MainBtn.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
-    const props = defineProps<{ 
-        disabled: boolean
-        onClick: () => void
-        srOnly: string
-        text: string
-        iconClass: string
-    }>()
+  const props = defineProps<{ 
+    disabled: boolean
+    onClick: () => void
+    srOnly: string
+    text: string
+    iconClass: string
+  }>()
 
-    const handleClick = () => {
-        if (!props.disabled) props.onClick()
-    }
+  const handleClick = () => {
+    if (!props.disabled) props.onClick()
+  }
 </script>
 <template>
-    <button
-        :disabled="props.disabled"
-        :aria-disabled="props.disabled"
-        @click="handleClick"
-        :class="[
-            'flex flex-row gap-2 bg-gray-300 text-stone-800 font-bold py-2 px-4 rounded items-center',
-            props.disabled ? 'opacity-20' : 'hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 cursor-pointer'
-        ]"
-        role="button"
-        :tabindex="props.disabled ? -1 : 0"
-        type="button"
-    >
-        <i :class="['text-xl pi', iconClass]" aria-hidden="true"></i>
-        <span class="sr-only">{{ srOnly }}</span>
-        <span aria-hidden="true">{{ text }}</span>
+  <button
+    :disabled="props.disabled"
+    :aria-disabled="props.disabled"
+    @click="handleClick"
+    :class="[
+      'flex flex-row gap-2 bg-gray-300 text-stone-800 font-bold py-2 px-4 rounded items-center',
+      props.disabled ? 'opacity-20' : 'hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 cursor-pointer'
+    ]"
+    role="button"
+    :tabindex="props.disabled ? -1 : 0"
+    type="button"
+  >
+    <i :class="['text-xl pi', iconClass]" aria-hidden="true"></i>
+    <span class="sr-only">{{ srOnly }}</span>
+    <span aria-hidden="true">{{ text }}</span>
   </button>
 </template>
